55ab6337dd6e9d82bce10aa0b27e09752de40ba7
Esther Okusanya@ <estherbokusanya@gmail.com>
2021-10-27 18:15:28 -0700

    Log in user after sucessful sign up

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -2,12 +2,21 @@
 from django.test import TestCase
 from django.urls import reverse
 from microblogs.forms import LogInForm
+from microblogs.models import User
+from .helpers import LogInTester
 
-class LogInViewTestCase(TestCase):
+class LogInViewTestCase(TestCase, LogInTester):
     """Tests of the sign up view."""
 
     def setUp(self):
         self.url = reverse('log_in')
+        User.objects.create_user('@johndoe',
+            first_name= 'John',
+            last_name= 'Doe',
+            email= 'johndoe@example.org',
+            bio= 'Hello, I am John Doe',
+            password= 'Password123',
+            )
 
     def test_log_in_url(self):
         self.assertEqual(self.url,'/log_in/')
@@ -19,3 +28,21 @@ class LogInViewTestCase(TestCase):
         form = response.context['form']
         self.assertTrue(isinstance(form, LogInForm))
         self.assertFalse(form.is_bound)
+
+    def test_unsucessful_log_in(self):
+        form_input = { 'username' : '@johndoe', 'password': 'WrongPassword123'}
+        response = self.client.post(self.url, form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+
+    def test_successul_log_in(self):
+        form_input = { 'username' : '@johndoe', 'password': 'Password123'}
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertTrue(self._is_logged_in())
+        response_url = reverse('feed')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
--- microblogs/views.py
+++ microblogs/views.py
@@ -1,8 +1,18 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 from .forms import LogInForm, SignUpForm
 
 
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get('username')
+            password= form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
     form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
 
@@ -16,7 +26,8 @@ def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request,user)
             return redirect('feed')
     else:
         form = SignUpForm()
