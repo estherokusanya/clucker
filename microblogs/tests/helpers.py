7317855b262f183b8ec82dc95eded339c2be34eb
Esther Okusanya@ <estherbokusanya@gmail.com>
2021-11-29 09:38:35 -0800

    Add users own posts to their feed

--- microblogs/tests/helpers.py
+++ microblogs/tests/helpers.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from microblogs.models import Post
 
 def reverse_with_next(url_name, next_url):
     url = reverse(url_name)
@@ -6,7 +7,11 @@ def reverse_with_next(url_name, next_url):
     return url
 
 
-
+def create_posts(author, from_count, to_count):
+    for count in range(from_count, to_count):
+        text = f'Post__{count}'
+        post = Post(author=author, text=text)
+        post.save()
 
 class LogInTester:
         def _is_logged_in(self):
